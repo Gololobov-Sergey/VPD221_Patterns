45d353884efa9bcc18fc4344889be765a4e4d97e
Сергій Гололобов <gololobov.serhiy@gmail.com>
2023-03-12 18:17:13 +0200

    12.03.2023

--- VPD221_Patterns/Bridge.h
+++ VPD221_Patterns/Bridge.h
@@ -103,3 +103,30 @@ public:
 		cout << "Отримано ЗП в за виконану роботу" << endl;
 	}
 };
+
+
+//int main()
+//{
+//	SetConsoleOutputCP(1251);
+//
+//	Programmer* pr1 = new OfficeProgrammer(new CPP_Language());
+//	pr1->work();
+//	pr1->getMoney();
+//	cout << endl;
+//
+//	pr1->setLanguage(new CSharp_Language());
+//	pr1->work();
+//	pr1->getMoney();
+//	cout << endl;
+//
+//
+//	Programmer* pr2 = new FreelanceProgrammer(new CPP_Language());
+//	pr2->work();
+//	pr2->getMoney();
+//	cout << endl;
+//
+//	pr2->setLanguage(new CSharp_Language());
+//	pr2->work();
+//	pr2->getMoney();
+//	cout << endl;
+//}
--- VPD221_Patterns/Source.cpp
+++ VPD221_Patterns/Source.cpp
@@ -20,24 +20,5 @@ int main()
 {
 	SetConsoleOutputCP(1251);
 
-	Programmer* pr1 = new OfficeProgrammer(new CPP_Language());
-	pr1->work();
-	pr1->getMoney();
-	cout << endl;
 
-	pr1->setLanguage(new CSharp_Language());
-	pr1->work();
-	pr1->getMoney();
-	cout << endl;
-
-
-	Programmer* pr2 = new FreelanceProgrammer(new CPP_Language());
-	pr2->work();
-	pr2->getMoney();
-	cout << endl;
-
-	pr2->setLanguage(new CSharp_Language());
-	pr2->work();
-	pr2->getMoney();
-	cout << endl;
 }
